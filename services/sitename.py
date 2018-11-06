3d461dd5252c77c3c35a8dea34a2a203a719f95c
Jake Bladt <jake@jakebladt.com>
2018-11-06 16:12:53 -0500

    +trap div/0

--- services/sitename.py
+++ services/sitename.py
@@ -4,14 +4,14 @@ import json
 def lambda_handler(event, context):
     number1 = event['Number1']
     number2 = event['Number2']
-    sum = number1 + number2
+    my_sum = number1 + number2
     product = number1 * number2
     difference = abs(number1 - number2)
-    quotient = number1 / number2
+    quotient = 'NaN' if number2 == 0 else number1 / number2
     return {
         "number1": number1,
         "number2": number2,
-        "sum": sum,
+        "sum": my_sum,
         "product": product,
         "quotient": quotient
     }
